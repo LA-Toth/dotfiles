f2c5b7059372463b3ba27b6d845705a4bfea6700
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2025-06-29 19:32:37 +0200

    improved gitignore; obsidian.el

--- dot/emacs.d/lisp/tla/packs/obsidian.el
+++ dot/emacs.d/lisp/tla/packs/obsidian.el
@@ -9,18 +9,21 @@
   (global-obsidian-mode t)
   :custom
   ;; This directory will be used for `obsidian-capture' if set.
-  (obsidian-inbox-directory "000 Inbox")
+  (obsidian-inbox-directory "00 Inbox")
   (obsidian-wiki-link-create-file-in-inbox nil)
-  (obsidian-daily-notes-directory "002 Daily Notes")
-  (obsidian-templates-directory "Templates")
+  (obsidian-daily-notes-directory "01 Daily Notes")
+  (obsidian-templates-directory "95 Templates")
+  (obsidian-daily-note-template "Daily Note Template.md")
   :bind (:map obsidian-mode-map
 	      ;; Replace C-c C-o with Obsidian.el's implementation. It's ok to use another key binding.
 	      ("C-c C-o" . obsidian-follow-link-at-point)
 	      ;; Jump to backlinks
 	      ("C-c C-b" . obsidian-backlink-jump)
 	      ;; If you prefer you can use `obsidian-insert-link'
-	      ("C-c C-l" . obsidian-insert-wikilink)))
-
+	      ("C-c C-l" . obsidian-insert-wikilink)
+        ;; Templates
+        ("C-c C-t" . obsidian-apply-template)
+        ))
 
 (global-set-key (kbd "C-c o j") 'obsidian-jump)
 (global-set-key (kbd "C-c o c") 'obsidian-capture)
--- dot/emacs.d/lisp/tla/ui.el
+++ dot/emacs.d/lisp/tla/ui.el
@@ -1,5 +1,5 @@
 (setq inhibit-startup-message t)
-(set-face-attribute 'default nil :font "Fira Code" :height 100)
+;(set-face-attribute 'default nil :font "Fira Code" :height 100)
 
 (load-theme 'wombat)
 
